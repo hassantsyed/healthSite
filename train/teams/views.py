fee3008c6bf3fb620f6408b8878d849ccb5dcef6
Hassan Syed <syedh@lafayette.edu>
2017-03-03 23:37:28 -0500

    browseing functionality
    not very DRY, can further abstract buttons (SHOULD DO THIS)
    potentially do with 1 template but probably need JS for that. Look into
    this later

--- train/teams/views.py
+++ train/teams/views.py
@@ -8,9 +8,20 @@ def profile(request):
     user = request.user
     return render(request, 'teams/profile.html', {'user':user})
 @login_required
-def browse(request):
-    return render(request, 'teams/browse.html')
+def browse_trainers(request):
+    trainers = User.objects.filter(area="TRAIN")
+    return render(request, 'teams/browse_trainers.html',{'trainers':trainers})
 @login_required
 def person(request, username):
     p = get_object_or_404(User, username=username)
     return render(request, 'teams/person.html', {'person':p})
+
+@login_required
+def browse_doctors(request):
+    doctors = User.objects.filter(area="MD")
+    return render(request, 'teams/browse_doctors.html',{'doctors':doctors})
+
+@login_required
+def browse_nutritionists(request):
+    nutritionists = User.objects.filter(area="NUTRITION")
+    return render(request, 'teams/browse_nutritionists.html',{'nutritionists':nutritionists})
