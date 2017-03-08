3d73efd7e2eddd0639984fe780fc9aec69e35fec
Hassan Syed <syedh@lafayette.edu>
2017-03-08 16:12:46 -0500

    username form validation
    using jquery and ajax

--- train/accounts/urls.py
+++ train/accounts/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     url(r'^login/', views.loginview, name='login'),
     url(r'^info/', views.info, name='info'),
     url(r'^logout/', views.logoutview, name='logout'),
+    url(r'^checkUser/', views.checkUser),
 ]
